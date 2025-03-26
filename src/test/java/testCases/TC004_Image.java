f34eff30f84b5f590da9b410fe3fcb9305cdcdd6
Sreekanth <sreekanth.erra516@gmail.com>
2025-03-26 13:18:08 +0530

    First commit

--- src/test/java/testCases/TC004_Image.java
+++ src/test/java/testCases/TC004_Image.java
@@ -1,5 +1,24 @@
 package testCases;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class TC004_Image {
+	
+	static void Read() throws IOException
+	{
+	
+	FileInputStream file = new FileInputStream ("/framework/testdata/Opencart Testdata_LoginData.xlsx");
+	XSSFWorkbook workbook = new XSSFWorkbook(file);
+	XSSFSheet sheet = workbook.getSheetAt(0);
+	String cellValue = sheet.getRow(1).getCell(0).getStringCellValue();
+	System.out.println(cellValue);
+	file.close();
+	
+	}
+	
 
 }
